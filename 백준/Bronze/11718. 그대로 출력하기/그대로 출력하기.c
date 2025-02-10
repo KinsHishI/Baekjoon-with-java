0dc6bbfc801a8bb918905294859650942426546a
Lee-Jeong-min <114851063+KinsHishI@users.noreply.github.com>
2025-02-11 02:58:10 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+	char a;
+
+	while (scanf("%c", &a) != -1)
+		printf("%c", a);
+
+	return 0;
+}
